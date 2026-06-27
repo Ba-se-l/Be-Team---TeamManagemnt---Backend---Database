123283b6b6381e5635dd567c7796b5a53555c09d
Ba-se-l <baselskaiker21@gmail.com>
2026-06-27 08:19:02 +0200

    "Delete init.py file from src/database"

--- src/database/init.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-from src.database.base import Base
-
-engine = create_engine("sqlite:///test.db", echo=True)
-
-def create_database():
-    Base.metadata.create_all(bind=engine)
-    
